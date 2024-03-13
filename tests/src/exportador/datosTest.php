99b26e33158949d84026dd0d413037553e9e2cc4
mgamboa <gamboa.martin@gmail.com>
2024-03-13 12:48:27 -0600

    v5.0.0 Se integra test 10

--- tests/src/exportador/datosTest.php
+++ tests/src/exportador/datosTest.php
@@ -9,9 +9,9 @@ use PhpOffice\PhpSpreadsheet\Spreadsheet;
 
 class datosTest extends test {
     public errores $errores;
-    public function __construct(?string $name = null, array $data = [], $dataName = '')
+    public function __construct(?string $name = null)
     {
-        parent::__construct($name, $data, $dataName);
+        parent::__construct($name);
         $this->errores = new errores();
     }
 
